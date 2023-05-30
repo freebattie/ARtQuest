3e191b4394100f90b7f8c438bcc693f1f80f002b
bjarte <22171847+freebattie@users.noreply.github.com>
2023-05-30 14:43:26 +0200

    Hard reset master

--- Pages/Login.js
+++ Pages/Login.js
@@ -1,52 +1,52 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState } from 'react';
 import {
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  SafeAreaView,
-  Button,
-} from "react-native";
+    StyleSheet,
+    Text,
+    View,
+    TextInput,
+    SafeAreaView,
+    Button,
+} from 'react-native';
 
-import { Appcontext } from "../lib/AppContext";
+import { Appcontext } from '../lib/AppContext';
 export default function Login({ navigation }) {
-  const [userName, setUserName] = useState("");
-  const [password, sePassword] = useState("");
-  const { login } = useContext(Appcontext);
-  const handelLogin = async () => {
-    try {
-      await login({ userName, password });
-      navigation.navigate("Home", { pass: password, user: userName });
-    } catch (error) {
-      console.log("handelLogin", error);
-    }
-  };
-  return (
-    <View style={styles.container}>
-      <Text>Insert any text in below input</Text>
-      <TextInput
-        value={userName}
-        onChangeText={(userName) => setUserName(userName)}
-        placeholder={"UserName"}
-        style={styles.input}
-      />
-      <Text>Insert any text in below input</Text>
-      <TextInput
-        value={password}
-        onChangeText={(password) => sePassword(password)}
-        placeholder={"Password"}
-        style={styles.input}
-      />
-      <Button title="login" onPress={() => handelLogin()} />
-      <Button title="create" />
-    </View>
-  );
+    const [userName, setUserName] = useState('');
+    const [password, sePassword] = useState('');
+    const { login } = useContext(Appcontext);
+    const handelLogin = async () => {
+        try {
+            await login({ userName, password });
+            navigation.navigate('Home', { pass: password, user: userName });
+        } catch (error) {
+            console.log('handelLogin', error);
+        }
+    };
+    return (
+        <View style={styles.container}>
+            <Text>Username</Text>
+            <TextInput
+                value={userName}
+                onChangeText={(userName) => setUserName(userName)}
+                placeholder={'UserName'}
+                style={styles.input}
+            />
+            <Text>Password</Text>
+            <TextInput
+                value={password}
+                onChangeText={(password) => sePassword(password)}
+                placeholder={'Password'}
+                style={styles.input}
+            />
+            <Button title="login" onPress={() => handelLogin()} />
+            <Button title="create" />
+        </View>
+    );
 }
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
