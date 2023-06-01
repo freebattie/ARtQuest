784dee6372cd57d8372774d89950eb7b56f2a643
Aixufey <jack_11728@msn.com>
2023-06-01 09:29:44 +0200

    Testing camera

--- Pages/Camera.js
+++ Pages/Camera.js
@@ -68,3 +68,30 @@ const styles = StyleSheet.create({
 
 
 
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    camera: {
+        flex: 1,
+        height: '100%',
+    },
+    buttonContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        backgroundColor: 'transparent',
+        margin: 64,
+    },
+    button: {
+        flex: 1,
+        alignSelf: 'flex-end',
+        alignItems: 'center',
+    },
+    text: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: 'white',
+    },
+});
