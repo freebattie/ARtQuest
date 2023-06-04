0e8b54cc5bb034ff99ce39ec915aea6a896efc31
Aixufey <jack_11728@msn.com>
2023-06-04 19:24:27 +0200

    Added SafeAreaProvider to correctly fit all screen

--- App.js
+++ App.js
@@ -1,45 +1,49 @@
-import { StatusBar } from "expo-status-bar";
+// Native
 import { StyleSheet } from "react-native";
+import {
+  SafeAreaProvider,
+} from "react-native-safe-area-context";
+
+// EXPO
+import { StatusBar } from "expo-status-bar";
+
+// Screens
 import Login from "./Pages/Login";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "./Pages/Home";
 import SignUp from "./Pages/SignUp";
 import CameraScreen from "./Pages/Camera";
+
+// Navigation
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+// Design
 import DesignSystem from "./components/style/DesignSystem";
-import { Image, View } from 'react-native';
 import useFont from "./components/hooks/useFont";
 
-const Stack = createNativeStackNavigator();
 export default function App() {
   const isFontLoaded = useFont();
   const { COLOR, PRIMARY, STACK_NAV_HEADER } = DesignSystem();
-  const pinkGreen = PRIMARY.COLOUR_ON_COLOUR.RED_BLACK;
+  const Stack = createNativeStackNavigator();
   const options = STACK_NAV_HEADER
 
   ////////////////////////////////////////////////////////////////////////
-  // Font loading
+  // Font has to be loaded before the app can be rendered
   if (isFontLoaded) {
     return (
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={options}>
-          <Stack.Screen name="Login" component={Login} />
-          <Stack.Screen name="Home" component={Home} options={options} />
-          <Stack.Screen name="SignUp" component={SignUp} options={options} />
-          <Stack.Screen name="Camera" component={CameraScreen} options={options} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <Stack.Navigator screenOptions={options} >
+            <Stack.Screen name="Login" component={Login} />
+            <Stack.Screen name="Home" component={Home} options={options} />
+            <Stack.Screen name="SignUp" component={SignUp} options={options} />
+            <Stack.Screen name="Camera" component={CameraScreen} options={options} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
     );
   } else {
     return null;
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
--- Pages/Login.js
+++ Pages/Login.js
@@ -1,21 +1,28 @@
 import React, { useContext, useState } from 'react';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import {
     StyleSheet,
     Text,
     View,
     TextInput,
-    SafeAreaView,
     Button,
 } from 'react-native';
 import designSystem from '../components/style/DesignSystem';
-
 import { Appcontext } from '../lib/AppContext';
 import CustomButton from '../components/style/CustomButton';
+
+
+
+
+
+const { COLOR } = designSystem();
+
 export default function Login({ navigation }) {
     const [userName, setUserName] = useState('');
     const [password, setPassword] = useState('');
     const { login } = useContext(Appcontext);
-    const handelLogin = async () => {
+    
+    const handleLogin = async () => {
         try {
             await login({ userName, password });
             navigation.navigate('Camera');
@@ -23,18 +30,43 @@ export default function Login({ navigation }) {
             console.log('handelLogin', error);
         }
     };
-
+    
     // Route to SignUp screen
     const handleCreateUser = () => {
         navigation.navigate('SignUp');
     };
-
+    
     const handleOpenCamera = () => {
         navigation.navigate('Camera');
     };
-
-    const { PRIMARY } = designSystem();
-    const pinkGreenStyle = PRIMARY.COLOUR_ON_COLOUR.PINK_GREEN;
+    
+    // hook for safe view
+    // TODO: refactor this into style in a context/provider?
+    const insets = useSafeAreaInsets();
+    const styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            backgroundColor: '#fff',
+            alignItems: 'center',
+            justifyContent: 'center',
+            paddingTop: insets.top,
+            paddingBottom: insets.bottom,
+            paddingLeft: insets.left,
+            paddingRight: insets.right,
+        },
+        input: {
+            width: '70%',
+            height: 40,
+            borderColor: 'rgba(50,50,50,1)',
+            borderWidth: 1.5,
+            borderRadius: 2,
+            paddingHorizontal: 10,
+            marginBottom: 10,
+        },
+        button: {
+            color: COLOR.MUNCH_BLACK,
+        }
+    });
     return (
         <View style={styles.container}>
             <TextInput
@@ -50,30 +82,11 @@ export default function Login({ navigation }) {
                 placeholder={'Password'}
                 style={styles.input}
             />
-            <CustomButton title={'Login'} onPress={() => alert('clicked')} />
-            <Button style={styles.button} title="login" onPress={() => handelLogin()} />
+
+            <CustomButton style={styles.button} title="login" onPress={() => handleLogin()} />
             <Button title="create" onPress={() => handleCreateUser()} />
             <Button title="camera" onPress={() => handleOpenCamera()} />
         </View>
     );
 }
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    input: {
-        width: '70%',
-        height: 40,
-        borderColor: 'rgba(50,50,50,1)',
-        borderWidth: 1.5,
-        borderRadius: 2,
-        paddingHorizontal: 10,
-        marginBottom: 10,
-    },
-    button: {
-        color: 'rgba(255,255,255)',
-    }
-});
+
