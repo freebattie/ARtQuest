eb8920e086c28df790bbe09d6cc79d52355e6503
Aixufey <jack_11728@msn.com>
2023-06-07 07:41:55 +0200

    Adding bottomTab - refactoring ongoing

--- App.js
+++ App.js
@@ -1,29 +1,37 @@
 // Native
-import { StyleSheet } from 'react-native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import {StyleSheet} from 'react-native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 // EXPO
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
 
 // Screens
-import Login from './Pages/Login';
-import Home from './Pages/Home';
-import SignUp from './Pages/SignUp';
+import LoginScreen from './Pages/Login';
+import HomeScreen from './Pages/Home';
+import SignUpScreen from './Pages/SignUp';
 import CameraScreen from './Pages/Camera';
 
 // Navigation
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 
 // Design
 import DesignSystem from './components/style/DesignSystem';
 import useFont from './components/hooks/useFont';
 
+// Navigators
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+// Tab Icon
+import {FontAwesome} from '@expo/vector-icons';
+
 export default function App() {
     const isFontLoaded = useFont();
-    const { COLOR, PRIMARY, STACK_NAV_HEADER } = DesignSystem();
-    const Stack = createNativeStackNavigator();
+    const {COLOR, PRIMARY, STACK_NAV_HEADER} = DesignSystem();
     const options = STACK_NAV_HEADER;
+    const BottomTab = createBottomTabNavigator();
 
     ////////////////////////////////////////////////////////////////////////
     // Font has to be loaded before the app can be rendered
@@ -31,28 +39,29 @@ export default function App() {
         return (
             <SafeAreaProvider>
                 <NavigationContainer>
-                    <StatusBar hidden />
-                    <Stack.Navigator
-                        headerShown={false}
-                        screenOptions={options}
-                    >
-                        <Stack.Screen name="Login" component={Login} />
-                        <Stack.Screen
-                            name="Home"
-                            component={Home}
-                            options={options}
-                        />
-                        <Stack.Screen
-                            name="SignUp"
-                            component={SignUp}
-                            options={options}
-                        />
-                        <Stack.Screen
-                            name="Camera"
-                            component={CameraScreen}
-                            options={options}
-                        />
-                    </Stack.Navigator>
+                    <StatusBar hidden/>
+                    <RootNavigator />
+                    {/*<Stack.Navigator*/}
+                    {/*    headerShown={false}*/}
+                    {/*    screenOptions={options}*/}
+                    {/*>*/}
+                    {/*    <Stack.Screen name="Login" component={Login}/>*/}
+                    {/*    <Stack.Screen*/}
+                    {/*        name="Home"*/}
+                    {/*        component={Home}*/}
+                    {/*        options={options}*/}
+                    {/*    />*/}
+                    {/*    <Stack.Screen*/}
+                    {/*        name="SignUp"*/}
+                    {/*        component={SignUp}*/}
+                    {/*        options={options}*/}
+                    {/*    />*/}
+                    {/*    <Stack.Screen*/}
+                    {/*        name="Camera"*/}
+                    {/*        component={CameraScreen}*/}
+                    {/*        options={options}*/}
+                    {/*    />*/}
+                    {/*</Stack.Navigator>*/}
                 </NavigationContainer>
             </SafeAreaProvider>
         );
@@ -60,3 +69,42 @@ export default function App() {
         return null;
     }
 }
+
+function RootNavigator() {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen
+                name="Login"
+                component={LoginScreen}
+                options={{headerShown: false}}
+            />
+            <Stack.Screen
+                name="Home"
+                component={HomeScreen}
+                options={{headerShown: false}}
+            />
+            <Stack.Screen
+                name="Camera"
+                component={BottomNavigator}
+                options={{headerShown: false}}
+            />
+        </Stack.Navigator>
+    )
+}
+
+function BottomNavigator() {
+    return (
+        <Tab.Navigator initialRouteName="Login">
+            <Tab.Screen
+                name="sdfasdfsdfsdfsafsdf"
+                component={CameraScreen}
+                options={{
+                    headerShown: false,
+                    tabBarIcon: ({color, size}) =>
+                        <FontAwesome name="home" size={size} color={color}/>
+                }}
+            />
+        </Tab.Navigator>
+    )
+}
+
