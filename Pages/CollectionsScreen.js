6dfd0d229daf2beaf57091250881f3731d167374
Michaël Mendes <30933848+Mimea005@users.noreply.github.com>
2023-06-08 18:42:51 +0200

    CollectionsScreen styling

--- Pages/CollectionsScreen.js
+++ Pages/CollectionsScreen.js
@@ -1,11 +1,11 @@
 import React, { useContext, useEffect, useState } from 'react';
 import {
-    StyleSheet,
-    Text,
-    View,
-    ScrollView,
-    Image,
-    Button,
+   StyleSheet,
+   Text,
+   View,
+   ScrollView,
+   Image,
+   Button,
 } from 'react-native';
 import { Appcontext } from '../lib/AppContext';
 import GalleryImageView from '../components/item/GalleryImageView';
@@ -13,146 +13,160 @@ import designSystem from '../components/style/DesignSystem';
 import CustomButton from '../components/style/CustomButton';
 
 export default function Collections({ navigation }) {
-    const { getAllUserRewards, getImageByName } = useContext(Appcontext);
-    const [rewardsArray, setRewardsArray] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [selectedReward, setSelectedReward] = useState({});
-    const [isSelected, setIsSelected] = useState(false);
+   const { getAllUserRewards, getImageByName } = useContext(Appcontext);
+   const [rewardsArray, setRewardsArray] = useState([]);
+   const [loading, setLoading] = useState(true);
+   const [selectedReward, setSelectedReward] = useState({});
+   const [isSelected, setIsSelected] = useState(false);
 
-    // Functions
-    const getUserRewards = async () => {
-        const data = await getAllUserRewards();
-        setLoading(false);
-        setRewardsArray(data);
-    };
+   // Functions
+   const getUserRewards = async () => {
+      const data = await getAllUserRewards();
+      setLoading(false);
+      setRewardsArray(data);
+   };
 
-    const selectImage = (reward) => {
-        console.log("Selected: ", reward);
-        return () => {
-            setSelectedReward(reward);
-            setIsSelected(true);
-        }
-    };
+   const selectImage = (reward) => {
+      console.log("Selected: ", reward);
+      return () => {
+         setSelectedReward(reward);
+         setIsSelected(true);
+      }
+   };
 
-    useEffect(() => {
-        getUserRewards();
-    }, []);
+   useEffect(() => {
+      getUserRewards();
+   }, []);
 
-    if (isSelected) {
-        const image = getImageByName(selectedReward.filename);
-        console.log("I am ", selectedReward.picturetitle);
-        console.log("My histoy is ", selectedReward.picturedescription)
-        return (<>
-            <ScrollView
+   if (isSelected) {
+      const image = getImageByName(selectedReward.filename);
+      console.log("I am ", selectedReward.picturetitle);
+      console.log("My histoy is ", selectedReward.picturedescription)
+      return (<>
+         <ScrollView
+         >
+            <Text
+               style={[
+                  designSystem().TEXT_STYLES.title,
+                  {
+                     paddingTop: "10%",
+                     paddingEnd: "10%",
+                     textAlign: 'right',
+                     flex: 1,
+                  }
+               ]}
             >
-                <Text
-                    style={[
-                        designSystem().TEXT_STYLES.title,
-                        {
-                            paddingTop: "10%",
-                            paddingEnd: "10%",
-                            textAlign: 'right',
-                            flex: 1,
-                        }
-                    ]}
-                >
-                    {selectedReward.picturetitle}
-                </Text>
-                <Image
-                    style={[
-                        {
-                            resizeMode: 'contain',
-                            width: '75%',
-                            marginTop: "-20%"
-                        },
-                    ]}
-                    source={image}
-                />
-                <Text
-                    style={[
-                        designSystem().TEXT_STYLES.text,
-                        {
-                            marginTop: "-15%",
-                            marginHorizontal: "15%"
-                        }
-                    ]}
-                >
-                    Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
-                    Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
-                    Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
-                    Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
-                    Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
-                    Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
-                    Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
-                    Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
-                    Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
-                    Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
-                    Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
-                    Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
-                    Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
-                    Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
-                    Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
-                    Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
-                </Text>
-            </ScrollView>
-            <CustomButton
-                title='Gallery'
-                onPress={() => setIsSelected(false)}
-                style={[
-                    designSystem().STYLING.primaryButtonText,
-                    {
-                        // display: 'none',
-                        position: 'absolute',
-                        bottom: 0,
-                    }
-                ]}
+               {selectedReward.picturetitle}
+            </Text>
+            <Image
+               style={[
+                  {
+                     resizeMode: 'contain',
+                     width: '75%',
+                     marginTop: "-20%"
+                  },
+               ]}
+               source={image}
             />
-        </>
-        );
-    }
+            <Text
+               style={[
+                  designSystem().TEXT_STYLES.text,
+                  {
+                     marginTop: "-15%",
+                     marginHorizontal: "15%"
+                  }
+               ]}
+            >
+               Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
+               Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
+               Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
+               Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
+               Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
+               Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
+               Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
+               Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
+               Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
+               Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
+               Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
+               Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
+               Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
+               Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
+               Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
+               Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
+            </Text>
+         </ScrollView>
+         <CustomButton
+            title='Gallery'
+            onPress={() => setIsSelected(false)}
+            style={[
+               designSystem().STYLING.primaryButtonText,
+               {
+                  position: 'absolute',
+                  bottom: 0,
+               }
+            ]}
+         />
+      </>
+      );
+   }
 
-    if (loading) {
-        console.log('I am loading');
-        return (
-            <View style={styles.container}>
-                <Text
-                    style={[
-                        styles.container,
-                        { color: 'magenta', backgroundColor: '#ff0000' },
-                    ]}
-                >
-                    Loading...
-                </Text>
-            </View>
-        );
-    }
-    console.log('i finished');
-    return (
-        <View>
-            <ScrollView style={styles.scrollView}>
-                {rewardsArray.map((reward, index) => {
-                    console.log("displaing reward: ", reward);
-                    return (
-                        <GalleryImageView
-                            key={index}
-                            reward={reward}
-                            onNavigate={selectImage(reward)}
-                        />
-                    );
-                })}
-            </ScrollView>
-        </View>
-    );
+   if (loading) {
+      console.log('I am loading');
+      return (
+         <View style={[
+            designSystem().CONTAINERS.container
+         ]}>
+            <Text
+               style={[
+                  styles.container,
+                  { color: 'magenta', backgroundColor: '#ff0000' },
+               ]}
+            >
+               Loading...
+            </Text>
+         </View>
+      );
+   }
+   console.log('i finished');
+   return (
+      <View style={[
+         designSystem().CONTAINERS.container
+      ]}>
+         <ScrollView
+            style={[
+               {
+                  display: 'flex',
+               }
+            ]}
+         >
+            {rewardsArray.map((reward, index) => {
+               console.log("displaing reward: ", reward);
+               return (
+                  <GalleryImageView
+                     key={index}
+                     reward={reward}
+                     onNavigate={selectImage(reward)}
+                     style={[
+                        {
+                           flex: 1,
+                        }
+                     ]}
+                  />
+               );
+            })}
+         </ScrollView>
+      </View>
+   );
 }
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    scrollView: {
-        backgroundColor: 'magenta',
-        marginHorizontal: 20,
-    },
+   container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+   },
+   scrollView: {
+      backgroundColor: 'magenta',
+      marginHorizontal: 20,
+   },
 });
