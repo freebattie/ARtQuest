168febac82ac56c1b3208ba9dc42f76e628fbdcd
bjarte <22171847+freebattie@users.noreply.github.com>
2023-06-12 13:36:29 +0200

    add documataation to methods

--- components/item/GalleryImageView.js
+++ components/item/GalleryImageView.js
@@ -1,26 +1,35 @@
 import React, { useState, useContext } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, Image, Pressable } from 'react-native';
+import {
+    View,
+    Text,
+    TouchableOpacity,
+    StyleSheet,
+    Image,
+    Pressable,
+} from 'react-native';
 import { Appcontext } from '../../lib/AppContext';
 
+/**
+ * show a singel picture and info
+ * @param reward - the obj containing info on picture
+ * @param onNavigate - used to navigate to next page
+ */
 export default function GalleryImageView({ reward, onNavigate }) {
-   const { getImageByName } = useContext(Appcontext);
-   console.log("GalleryImageView");
-   console.log("reward: ", reward);
+    const { getImageByName } = useContext(Appcontext);
+    console.log('GalleryImageView');
+    const image = getImageByName(reward.filename);
 
-   console.log("Accessing ", reward.filename)
-   const image = getImageByName(reward.filename);
-   console.log("got: ", image);
-   return (
-      <Pressable onPress={() => onNavigate()}>
-         <Image
-            source={image}
-            style={{
-               width: '100%',
-               height: undefined,
-               aspectRatio: 1,
-            }}
-         />
-         <Text>{reward.title}</Text>
-      </Pressable>
-   );
+    return (
+        <Pressable onPress={() => onNavigate()}>
+            <Image
+                source={image}
+                style={{
+                    width: '100%',
+                    height: undefined,
+                    aspectRatio: 1,
+                }}
+            />
+            <Text>{reward.title}</Text>
+        </Pressable>
+    );
 }
