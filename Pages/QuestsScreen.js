1e47d1e69c49b5a0adc2c5a6af8c28b743b9f5f3
bjarte <22171847+freebattie@users.noreply.github.com>
2023-06-15 10:59:06 +0200

    fix bugs

--- Pages/QuestsScreen.js
+++ Pages/QuestsScreen.js
@@ -4,42 +4,27 @@
 //  Author: Bjarte
 //  Co-author: Jack
 ///////////////////////////////////////////////////////////////
-import {
-    StyleSheet,
-    View,
-} from 'react-native';
-import QuestProgressList from "../components/item/QuestProgressList";
-import designSystem from "../components/style/DesignSystem";
+import { StyleSheet, View } from 'react-native';
+import QuestProgressList from '../components/item/QuestProgressList';
+import designSystem from '../components/style/DesignSystem';
 import CustomButton from '../components/style/CustomButton';
-
-
-
-
-
-
 
 /**
  * QuestScreen is rendering a typical List-item pattern in react.
  * A list is rendering all items from the server.
  * Item is a card view component.
- * 
+ *
  * @param {*} navigation prop is provided by StackNavigator inside App.js in case you need to route forward.
  * @returns A Quest progression "page" that allows user to see their quests progression.
  */
 export default function QuestsScreen({ navigation }) {
-
-
     return (
-        <View style={[
-            designSystem().CONTAINERS.container
-        ]}>
+        <View style={[designSystem().CONTAINERS.container]}>
             <QuestProgressList />
             <CustomButton
                 title="Back"
                 onPress={() => navigation.pop()}
-                style={[
-                    designSystem().STYLING.primaryButton
-                ]}
+                style={[designSystem().STYLING.primaryButton]}
             />
         </View>
     );
